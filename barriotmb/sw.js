0df52e4b530f940723d468198e849f0785f74423
diegoperezmartinez85-art <diegoperezmartinez85@gmail.com>
2026-06-02 00:28:37 -0300

    Update sw.js

--- barriotmb/sw.js
+++ barriotmb/sw.js
@@ -1,6 +1,6 @@
 // ── NEXOS SERVICE WORKER ─────────────────────────────────
 // ⚠️ Cambiar este número CADA VEZ que subas un HTML nuevo
-const CACHE_NAME = 'nexos-v1.27';
+const CACHE_NAME = 'nexos-v1.28';
 
 const ARCHIVOS = [
   './nexos_panico.html',
